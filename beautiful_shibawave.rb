0b3bf9e8c75abc57ebdad4373ba7f43f76263564
shibayu36 <shibayu36@gmail.com>
2014-03-02 17:56:39 +0900

    コメントなどを書いてわかりやすくしました

--- beautiful_shibawave.rb
+++ beautiful_shibawave.rb
@@ -6,8 +6,10 @@
 t = 0.0
 speed = 0.2
 
+# 引数で指定したsize使います
 size = ARGV[0].to_i
 
+# 指定したサイズのシバッを表示
 def shiba!(level)
   if level == 0
     ""
@@ -23,8 +25,9 @@ loop do
   if sin >= 0
     puts "　" * size + shiba!(sin)
   else
-    bashi = shiba!(sin.abs).reverse
-    puts "　" * (size - bashi.size) + bashi
+    # 負の方向のグラフならシバッでなくてッバシと表示します
+    reverse_shiba = shiba!(sin.abs).reverse
+    puts "　" * (size - reverse_shiba.size) + reverse_shiba
   end
 
   sleep 0.03
