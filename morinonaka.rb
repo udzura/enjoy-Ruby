94ed209e85dd636b786780819a36a4c2690758a3
nyanco15 <nyannyanco15@gmail.com>
2014-03-02 17:49:19 +0900

    森の中で出会った・・・

---
+++ morinonaka.rb
@@ -0,0 +1,18 @@
+def yuuki
+  "ゆううき"
+end
+
+def catatsuy
+  "カタツイ"
+end
+
+def shibayu
+  "しばゆー"
+end
+
+def story(person1,person2)
+  "ある日森で#{person1}と#{person2}に出会いました。"
+end
+puts story(shibayu,yuuki)
+puts story(yuuki,catatsuy)
+
