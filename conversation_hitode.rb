831cdcde683125eca8fa1505fa8a86ba0f9d21ff
nyanco15 <nyannyanco15@gmail.com>
2014-03-04 14:14:07 +0900

    社会の厳しさの
    hitode編

---
+++ conversation_hitode.rb
@@ -0,0 +1,37 @@
+person = ARGV[0]
+person1 = ARGV[1]
+
+def conversation1
+  "あなたはだれですか？"
+end
+
+def conversation2(person)
+  "私の名前は#{person}です"
+end
+
+def conversation3(person)
+   case person 
+   when "hitode909","ひとで","hitode"
+   "#{person}さんですか！よろしくお願いします！趣味はなんですか？"
+   else
+   "なるほど。よろしくお願いします。趣味はなんですか？"
+  end
+end
+
+#ここからしたわからん
+#ARGV[1]がかえって（？）こない、何故
+
+def conversation4(hobby)
+  case hobby 
+  when /"マリンスポーツ"/
+  "#{hobby}です！"
+  else
+  "おや？あなたは別人ですね？"
+ end
+end
+person = ARGV[0]
+hobby = ARGV[1]
+puts conversation1
+puts conversation2(person)
+puts conversation3(person)
+puts conversation4(hobby)
